243ab862edf6b72ce115a1978898c7c13fc0e761
AndyGG <109757479+AsuPlayz@users.noreply.github.com>
2024-03-02 03:28:09 +0530

    Updated

---
+++ config.lua
@@ -0,0 +1,19 @@
+Config = {}
+
+    Config.Blips = {
+    {   
+        id = 1,
+        name = "Lumber Jack",
+        -- sprite = 112,
+        -- color = 81,
+        -- alpha = 255,
+        -- scale = 1.0,
+        display = 4,
+        shortRange = true,
+        coords = vector3(-565.92, 5357.93, 70.19),
+        radius = 500.0,
+        radiusBlipColor = 2, 
+        radiusBlipAlpha = 100, 
+
+    },
+}
