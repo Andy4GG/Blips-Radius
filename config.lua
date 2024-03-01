7469fc94c4ac04ea4d09b9749c6831403c0ccdde
AndyGG <109757479+AsuPlayz@users.noreply.github.com>
2024-03-02 03:33:57 +0530

    Update.config.lua

--- config.lua
+++ config.lua
@@ -12,11 +12,11 @@ Config = {}
         -- alpha = 255,
         -- scale = 1.0,
         display = 4,
-        shortRange = true,
-        coords = vector3(-565.92, 5357.93, 70.19),
-        radius = 500.0,
-        radiusBlipColor = 2, 
-        radiusBlipAlpha = 100, 
+        shortRange = true, 
+        coords = vector3(-565.92, 5357.93, 70.19), -- Only Takes Vector3
+        radius = 500.0, -- How much Radius is Wide
+        radiusBlipColor = 2,  -- This is for Radius Color
+        radiusBlipAlpha = 100,  -- This is for Radius Opacity
 
     },
 
